9db51c9cccbaa8cf6d2a9e61c5e0e7da21ef1d48
Ken TY Chiu <ken.tychiu@gmail.com>
2020-12-14 01:16:33 +0800

    Update thumb shift config to align wordings with readme

--- keyboards/ml67/keymaps/thumb_shift/config.h
+++ keyboards/ml67/keymaps/thumb_shift/config.h
@@ -1,7 +1,7 @@
-/* Uncomment to define the combo time limit. Defaults to 100ms if not defined */
-/* ja:　コメント解除することで、同時打鍵判定の制限時間を定義できる。定義しない場合の初期値は100ミリ秒 */
+/* Uncomment to define the combo time limit. Defaults to 100ms */
+/* ja:　コメント解除することで、同時打鍵の制限時間（失敗判定猶予）を定義できる。デフォルトは100ミリ秒 */
 // #define COMBO_TERM 100
 
-/* Uncomment to define the minimum time difference between combos (overlap duration). Defaults to 2 x DEBOUNCE if not defined */
-/* ja: コメント解除することで、同時打鍵の最小間隔（重なり厚み）を定義できる。定義しない場合の初期値は 2 × DEBOUNCE */
+/* Uncomment to define the minimum time difference between combos (overlap duration). Defaults to 2 x DEBOUNCE */
+/* ja: コメント解除することで、同時打鍵の重なり判定時間 (最小間隔）を定義できる。デフォルトは 2 × DEBOUNCE */
 // #define OVERLAP_TERM (2 * DEBOUNCE)
